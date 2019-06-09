ddcb4277bcbb20bbda5127cb9a7e50278180829e
Arthur <rthurmedeiros@gmail.com>
2019-06-09 15:38:24 -0300

    Constante para controlar debug

--- sorting.c
+++ sorting.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 #define MAX_ARRAY_SIZE 50
+#define DEBUG 1
 
 void printArray(int vetor[], int tamanho, int indexA, int indexB)
 {
@@ -8,9 +9,9 @@ void printArray(int vetor[], int tamanho, int indexA, int indexB)
     for (i = 0; i < tamanho; i++)
     {
         if (i == indexA || i == indexB)
-            printf("[%i] ", vetor[i]);
+            printf("[%i]", vetor[i]);
         else
-            printf(" %i  ", vetor[i]);
+            printf(" %i ", vetor[i]);
     }
     printf("\n");
 }
@@ -32,24 +33,36 @@ void shellSort(int vetor[], int tamanho)
     // Loop de alteração do incremento de shell
     while (h > 0)
     {
-        printf("\nh: %i\narray:\t", h);
-        printArray(vetor, tamanho, -1, -1);
+        if (DEBUG)
+        {
+            printf("\nh: %i\narray:\t", h);
+            printArray(vetor, tamanho, -1, -1);
+        }
         // Avança o indice da direita
         for (i = h; i < tamanho; i++)
         {
             aux = vetor[i];
             j = i; // j parte da posição i
-            printf("\t");
-            printArray(vetor, tamanho, i - h, i);
+            if (DEBUG)
+            {
+                printf("\t");
+                printArray(vetor, tamanho, i - h, i);
+            }
             // Fica em loop até encontrar a posição adequada
             while (j > h - 1               // j - h precisa estar no vetor
                    && vetor[j - h] >= aux) // j - h precisa ser menor que o valor a ser movido
             {
-                printf("%i > %i\t", vetor[j - h], aux);
+                if (DEBUG)
+                    printf("%i > %i\t", vetor[j - h], aux);
+
                 vetor[j] = vetor[j - h]; // Copia o valor anterior pro indice candidato
                 j = j - h;               // Move o indice candidato h posição para trás
-                // vetor[j] = aux; // DEBUG
-                printArray(vetor, tamanho, j, i);
+
+                if (DEBUG)
+                {
+                    vetor[j] = aux;
+                    printArray(vetor, tamanho, j, i);
+                }
             }
             // Atribui o valor à nova posição
             vetor[j] = aux;
@@ -65,7 +78,8 @@ int main()
     int array[MAX_ARRAY_SIZE], t = 0, in = 0;
     printf("Insira números para o array a ser ordenado: (-1 para terminar)\n");
     scanf("%i", &in);
-    while (t < MAX_ARRAY_SIZE && in >= 0) {
+    while (t < MAX_ARRAY_SIZE && in >= 0)
+    {
         array[t] = in;
         t++;
         scanf("%i", &in);
